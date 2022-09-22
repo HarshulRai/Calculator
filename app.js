d1b506d44b884482522ecb558cc4779520b3ea9d
Harshul Rai <61915312+HarshulRai@users.noreply.github.com>
2022-09-23 02:39:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express")
+const app = express();
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+app.get("/", function (req, res) {
+    res.send(5);
+});
+
+app.post("/add", function (req, res) {
+    let a = parseInt(req.body.a);
+    let b = parseInt(req.body.b);
+    let ans = a + b;
+    res.send(JSON.stringify({ ans: ans }));
+});
+
+app.post("/div", function (req, res) {
+    let a = parseInt(req.body.a);
+    let b = parseInt(req.body.b);
+    let ans = a / b;
+    res.send(JSON.stringify({ ans: ans }));
+});
+
+app.post("/sub", function (req, res) {
+    let a = parseInt(req.body.a);
+    let b = parseInt(req.body.b);
+    let ans = a - b;
+    res.send(JSON.stringify({ ans: ans }));
+});
+
+app.post("/mul", function (req, res) {
+    let a = parseInt(req.body.a);
+    let b = parseInt(req.body.b);
+    let ans = a * b;
+    res.send(JSON.stringify({ ans: ans }));
+});
+
+app.listen(3000, function () {
+    console.log("Port 3000 working");
+});
